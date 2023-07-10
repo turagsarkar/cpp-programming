524fc115fd1959c61219d2980f613a1e503b23fd
Md.Abdullah Turag Sarkar <79811452+turagsarkar@users.noreply.github.com>
2023-07-11 05:19:25 +0600

    Add files via upload

---
+++ Datatype.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int a , b , c , sum;
+    cout<<"Size of varriable : "<<sizeof(a)<< endl;
+    cin>> b ;
+    cin>> c ; 
+    int sum= b+c ; 
+    cout<< sum ; 
+
+}
